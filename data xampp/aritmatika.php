14e6e692f9f4dee2fddc0ec0524007d3b618d851
Haski26 <113011929+Haski26@users.noreply.github.com>
2022-10-27 16:14:08 +0700

    Add files via upload

---
+++ data xampp/aritmatika.php
@@ -0,0 +1,16 @@
+<?php 
+
+$a=52;
+$b=21;
+
+$hasil1=$a-$b;
+$hasil2=$a*$b;
+$hasil3=$a+$b;
+$hasil4=$a/$b;
+
+echo"$a-$b = $hasil1<br>";
+echo"$a x $b = $hasil2<br>";
+echo"$a + $b = $hasil3<br>";
+echo"$a : $b = $hasil4<br>";
+
+?>
